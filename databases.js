eb3ca2f8de0df93176019339edde23ebaf6b148b
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-10-04 16:54:05 -0500

    Isolate selectors.

--- databases.js
+++ databases.js
@@ -29,46 +29,48 @@
                     title: "Databases",
                     content: "<div class=\"page-header\"><h1>Databases <small>Activate third-party databases for use with the TCGA Toolbox.</small></h1></div><p><span class=\"label label-info\">Info</span> This module allows you to activate or deactivate communication with databases other than TCGA. However, it will not add an API to those databases.</p><h2>UniProt</h2><p>UniProt is a comprehensive resource for protein sequence and annotation data.</p><button class=\"btn databases-toggle\" data-database-name=\"UniProt\" data-database-url=\"http://beta.sparql.uniprot.org/*\" class=\"btn\"></button>",
                     switchTab: true
-                });
+                }, function (el) {
 
-             // Update state of buttons.
-                $(".databases-toggle").each(function (i, button) {
-                    var database;
-                    database = button.dataset.databaseUrl;
-                    if (databases[database] === true) {
-                        button.innerText = "Deactivate " + button.dataset.databaseName;
-                    } else {
-                        button.innerText = "Activate " + button.dataset.databaseName;
-                    }
-                });
+                 // Update state of buttons.
+                    $(".databases-toggle", el).each(function (i, button) {
+                        var database;
+                        database = button.dataset.databaseUrl;
+                        if (databases[database] === true) {
+                            button.innerText = "Deactivate " + button.dataset.databaseName;
+                        } else {
+                            button.innerText = "Activate " + button.dataset.databaseName;
+                        }
+                    });
 
-                $(".databases-toggle").click(function (ev) {
-                    var button, database;
-                    button = ev.target;
-                    database = button.dataset.databaseUrl;
-                    if (databases[database] === true) {
-                        chrome.permissions.remove({
-                            origins: [database]
-                        }, function (removed) {
-                            if (removed === true) {
-                                databases[database] = false;
-                                button.innerText = "Activate " + button.dataset.databaseName;
-                            } else {
-                                console.err("Origin was not removed.");
-                            }
-                        });
-                    } else {
-                        chrome.permissions.request({
-                            origins: [database]
-                        }, function (granted) {
-                            if (granted === true) {
-                                databases[database] = true;
-                                button.innerText = "Deactivate " + button.dataset.databaseName;
-                            } else {
-                                console.err("Origin was not granted.");
-                            }
-                        });
-                    }
+                    $(".databases-toggle", el).click(function (ev) {
+                        var button, database;
+                        button = ev.target;
+                        database = button.dataset.databaseUrl;
+                        if (databases[database] === true) {
+                            chrome.permissions.remove({
+                                origins: [database]
+                            }, function (removed) {
+                                if (removed === true) {
+                                    databases[database] = false;
+                                    button.innerText = "Activate " + button.dataset.databaseName;
+                                } else {
+                                    console.err("Origin was not removed.");
+                                }
+                            });
+                        } else {
+                            chrome.permissions.request({
+                                origins: [database]
+                            }, function (granted) {
+                                if (granted === true) {
+                                    databases[database] = true;
+                                    button.innerText = "Deactivate " + button.dataset.databaseName;
+                                } else {
+                                    console.err("Origin was not granted.");
+                                }
+                            });
+                        }
+                    });
+
                 });
 
             });
